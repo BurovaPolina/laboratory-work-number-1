bf4875264f9da5fdf553eb03fb4f7d5e6202561c
BurovaPolina <128833381+BurovaPolina@users.noreply.github.com>
2023-05-19 22:04:19 +0300

    Update lab6.py

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -6,102 +6,71 @@
 В холодильнике 10 брикетов мороженого разного вида. Ребенку разрешается взять вечером не более 2 брикетов.
 Подготовьте различные варианты поедания мороженного ребенком на неделю."""
 
+import random
+
+
 def calculate_total_calories(combination):
     return sum(ice_cream[2] for ice_cream in combination)
 
 
-def find_optimal_combination(ice_creams):
-    min_calories = float('inf')
-    optimal_combination = []
-
-    for ice_cream in ice_creams:
-        total_calories = ice_cream[2]
-        if total_calories < min_calories:
-            min_calories = total_calories
-            optimal_combination = [ice_cream]
-
-    return optimal_combination
-
-
-def find_optimal_combination_target(ice_creams):
-    min_calories = float('inf')
-    optimal_combination = []
-
-    for i in range(1, len(ice_creams) + 1):
-        combinations = find_combinations(ice_creams, i)
-        for combination in combinations:
-            total_calories = calculate_total_calories(combination)
-            if total_calories < min_calories:
-                min_calories = total_calories
-                optimal_combination = combination
-
-    return optimal_combination
-
-
-def find_combinations(lst, k):
-    if k == 0:
-        return [[]]
-    if not lst:
-        return []
-
-    result = []
-    first = lst[0]
-    rest = lst[1:]
-    for comb in find_combinations(rest, k - 1):
-        result.append([first] + comb)
-    result.extend(find_combinations(rest, k))
-    return result
-
-
 def main():
     ice_creams = [
-        ("Брикет1", "качественное", 100),
-        ("Брикет2", "некачественное", 150),
-        ("Брикет3", "некачественное", 120),
-        ("Брикет4", "качественное", 80),
-        ("Брикет5", "некачественное", 110),
-        ("Брикет6", "качественное", 90),
-        ("Брикет7", "некачественное", 130),
-        ("Брикет8", "качественное", 70),
-        ("Брикет9", "некачественное", 140),
-        ("Брикет10", "качественное", 60),
+        ("Шоколад", "качественное", 100),
+        ("Банан", "некачественное", 150),
+        ("Клубника", "некачественное", 120),
+        ("Апельсин", "качественное", 80),
+        ("Ваниль", "некачественное", 110),
+        ("Лесные ягоды", "качественное", 90),
+        ("Облепиха", "некачественное", 130),
+        ("Смородина", "качественное", 70),
+        ("Крем-брюле", "некачественное", 140),
+        ("Сыр", "качественное", 60),
     ]
 
-    mode = input("Выберите режим программы: '1' или '2': ")
     days = 7
+    total_calories = 0
 
-    if mode == '1':
-        total_calories = 0
+    print("Берем мороженое без ограничения от 0 до 2")
 
-        for day in range(days):
-            optimal_combination = find_optimal_combination(ice_creams)
-            total_calories += calculate_total_calories(optimal_combination)
+    for day in range(days):
+        daily_portion = random.randint(0, 2)
+        print("День", day + 1)
+        # берем несколько из списка от 0 до 2
+        portion_list = ice_creams[:daily_portion]
+        # удаляем из списка то что съели
+        del ice_creams[:daily_portion]
+        print('список на день: ', portion_list)
+        daily_calories = calculate_total_calories(portion_list)
+        print("Количество калорий в день: ", daily_calories)
 
-            print("День", day + 1)
-            print(optimal_combination[0][0])
-            print("Калории:", calculate_total_calories(optimal_combination), '\n')
+        total_calories += daily_calories
 
-            ice_creams.remove(optimal_combination[0])
+    print("Общее количество потребленных калорий не благоприятный:", total_calories)
 
-        print("Общее количество потребленных калорий:", total_calories)
-    elif mode == '2':
-        total_calories = 0
+    total_calories = 0
 
-        for day in range(days):
-            optimal_combination = find_optimal_combination_target(ice_creams)
-            total_calories += calculate_total_calories(optimal_combination)
+    print("\n\n\nБерем одно из (от 0 до 2) мороженое с наименьшим колличеством каллорий")
 
-            print("День", day + 1)
-            for ice_cream in optimal_combination:
-                print(ice_cream[0])
-            print("Калории:", calculate_total_calories(optimal_combination), '\n')
+    for day in range(days):
+        daily_portion = random.randint(0, 2)
+        print("День", day + 1)
+        # берем несколько из списка от 0 до 2
+        portion_list = ice_creams[:daily_portion]
+        print('список на день: ', portion_list)
+        # если взяли 2 то предпочтение тому что с минимальными каллориями
+        if len(portion_list) > 1:
+            ice_cream = sorted(portion_list, key=lambda ice_cream: ice_cream[2])[0]
+            daily_calories = ice_cream[2]
+            # удаляем из списка то что съели
+            ice_creams.remove(ice_cream)
+        elif len(portion_list) == 1:
+            daily_calories = portion_list[0][2]
+        else:
+            daily_calories = 0
 
-            for ice_cream in optimal_combination:
-                ice_creams.remove(ice_cream)
+        print("Количество калорий в день: ", daily_calories)
+        total_calories += daily_calories
 
-        print("Общее количество потребленных калорий:", total_calories)
-    else:
-        print("Некорректный режим программы.")
+    print("Общее количество потребленных калорий не благоприятный:", total_calories)
 
-
-main()
+ main()
